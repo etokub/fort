f4fefe74c845e531a585787a0e65e5103e683a56
EtoKub <etokub@gmail.com>
2021-02-10 19:13:18 +0200

    15

--- libs/js/main.js
+++ libs/js/main.js
@@ -24,7 +24,7 @@ var scroll = new SmoothScroll('a[data-scroll]', {
   header: '[data-scroll-header]',
   offset: function (anchor, toggle) {
    {
-			return 50;
+			return 25;
 		}
 
 	},
@@ -99,7 +99,7 @@ var descrSlider = tns({
 
 
 const navTgl = document.querySelector("#nav-tgl");
-const navMobile = document.querySelector(".header-navigation");
+const navMobile = document.querySelector(".header__navigation");
 const navBtns = document.querySelectorAll(".header-nav__item a");
 const page = document.querySelector("html");
 
@@ -199,11 +199,29 @@ el.addEventListener('focus', function(event) {
 
 
 
-const answers = [];
-const previousButton = document.getElementById("prev-step");
-const nextButton = document.getElementById("next-step");
-const slides = document.querySelectorAll(".quiz-step");
-let currentSlide = 0;
+// var modalOpenBtns = modalOpen;
+// modalOpenBtns.forEach(function(btn){
+//   btn.onclick = function() {
+//     var v = btn.getAttribute('data-m-open');
+//     document.getElementById(v).classList.add('is-open');
+//     page.classList.add('no-scroll');
+//   }
+// });
+
+var carousels = document.querySelectorAll("[data-quiz-container]");
+ var carousel =  carousels
+
+carousel.forEach(function(el){
+
+  const slides = el.querySelectorAll(".quiz-step"); 
+  const previousButtons = el.querySelectorAll(".prev-step");
+  console.log(slides)
+  console.log(previousButtons)
+
+
+  let currentSlide = 0;
+
+  const pb = previousButtons;
 
 
 
@@ -222,9 +240,9 @@ function showSlide(n) {
   slides[n].classList.add('active');
   currentSlide = n;
 
-  var bar = document.querySelector(".quiz-progress__bar--line"),
-      counter = document.querySelector(".quiz-progress__counter"),
-      w  = document.querySelector(".quiz-progress").clientWidth,
+  var bar = el.querySelector(".quiz-progress__bar--line"),
+      counter = el.querySelector(".quiz-progress__counter"),
+      w  = el.querySelector(".quiz-progress").clientWidth,
       cw = counter.clientWidth / 2,
       s = slides.length,
       sl = w / s,
@@ -234,26 +252,36 @@ function showSlide(n) {
  
 
 
-  document.querySelector(".quiz-progress__counter--current").innerHTML
+  el.querySelector(".quiz-progress__counter--current").innerHTML
   = currentSlide + 1
-  document.querySelector(".quiz-progress__counter--length").innerHTML
+  el.querySelector(".quiz-progress__counter--length").innerHTML
   = slides.length
 
-  var str = Array.prototype.slice.call(document.querySelectorAll('input[data-answer]:checked')).map(function(el){
+  var str = Array.prototype.slice.call(el.querySelectorAll('input[data-answer]:checked')).map(function(el){
       return el.value;
   }).join(';');
 
-      document.getElementById('results').value = str;
-      var v =  document.getElementById('results').value
-      console.log(v)
+  console.log(str)
+      el.querySelector('.form-el--results').value = str;
+
+
 
   if(currentSlide === 0){
-    // previousButton.disabled = true;
-    previousButton.style.display = 'none';
+
+    pb.forEach(function(el){
+    el.style.display = 'none';
+    })
   }
   else{
-    // previousButton.disabled = false
-    previousButton.style.display = 'inline-block';
+    // previousButtons.disabled = false
+    // previousButtons.style.display = 'inline-block';
+  
+    pb.forEach(function(el){
+      el.style.display = 'inline-block';
+      })
+  
+    
+
   }
   if(currentSlide === slides.length-1){
     // nextButton.style.display = 'none';
@@ -267,13 +295,15 @@ function quizProgress (){
   
 }
 
-  showSlide(currentSlide);
+showSlide(currentSlide);
 
-  previousButton.addEventListener("click", showPreviousSlide);
-  // nextButton.addEventListener("click", showNextSlide);
+pb.forEach(function(el){
+  el.addEventListener("click", showPreviousSlide)
+  })
 
 
-  const question = document.querySelectorAll(".quiz-question");
+
+  const question = el.querySelectorAll(".quiz-question");
 var questions = question;
 questions.forEach(function(el){
   el.onclick = function() {
@@ -290,7 +320,124 @@ questions.forEach(function(el){
 });
 
 
-function displayRadioValue() { 
+});
+
+
+// (function() {
   
-} 
+//   var carousels = document.querySelectorAll('[data-quiz-container]');
+  
+//   [].forEach.call(carousels, function(carousel) {
+//     carouselize(carousel);
+//   });
+  
+// })();
+
+
+// function carouselize(carousel) {
+
+//   const answers = [];
+//   const previousButtons= document.querySelector("prev-step");
+//   const nextButton = document.querySelector(".next-step");
+//   const slides = document.querySelectorAll(".quiz-step"); 
+
+
+// let currentSlide = 0;
+
+
+
+
+// function showNextSlide() {
+//   showSlide(currentSlide + 1);
+// }
+
+// function showPreviousSlide() {
+//   showSlide(currentSlide - 1);
+// }
+
+
+// function showSlide(n) {
+//   slides[currentSlide].classList.remove('active');
+//   slides[n].classList.add('active');
+//   currentSlide = n;
+
+//   var bar = document.querySelector(".quiz-progress__bar--line"),
+//       counter = document.querySelector(".quiz-progress__counter"),
+//       w  = document.querySelector(".quiz-progress").clientWidth,
+//       cw = counter.clientWidth / 2,
+//       s = slides.length,
+//       sl = w / s,
+//       cs = n + 1;
+//   bar.style.width = sl * cs + "px";
+//   counter.style.transform = 'translateX(' + ((sl*cs)-cw) + 'px)';
+ 
+
+
+//   document.querySelector(".quiz-progress__counter--current").innerHTML
+//   = currentSlide + 1
+//   document.querySelector(".quiz-progress__counter--length").innerHTML
+//   = slides.length
+
+//   var str = Array.prototype.slice.call(document.querySelectorAll('input[data-answer]:checked')).map(function(el){
+//       return el.value;
+//   }).join(';');
+
+//       // document.getElementById('results').value = str;
+//       // var v =  document.getElementById('results').value
+
+
+//   if(currentSlide === 0){
+//     previousButtons.style.display = 'none';
+
+
+//   }
+//   else{
+//     // previousButtons.disabled = false
+//     // previousButtons.style.display = 'inline-block';
+  
+//     previousButtons.style.display = 'inline-block';
+    
+
+//   }
+//   if(currentSlide === slides.length-1){
+//     // nextButton.style.display = 'none';
+//   }
+//   else{
+//     // nextButton.style.display = 'inline-block';
+//   }
+// }
+
+// function quizProgress (){
+  
+// }
+
+//   showSlide(currentSlide);
+
+//   var pb  = previousButtons;
+//   pb.forEach(function(btn){
+//     btn.addEventListener("click", showPreviousSlide)
+//   });
+
+//   // pb.addEventListener("click", showPreviousSlide);
+//   // nextButton.addEventListener("click", showNextSlide);
+
+
+//   const question = document.querySelectorAll(".quiz-question");
+// var questions = question;
+// questions.forEach(function(el){
+//   el.onclick = function() {
+
+//     setTimeout(function() {
+//       showNextSlide(); 
+//       quizProgress()
+//       console.log('click')
+//   }, 500);
+
+   
+//   }
+
+// });
+
+
+// }
 
